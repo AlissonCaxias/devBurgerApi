8324f5e32efcbb07e646e9f751e711e1821a24f1
AlissonCaxias <alissondasilva241@gmail.com>
2026-07-20 00:38:50 -0300

    Criando a tabela de usuarios usando migration

---
+++ src/config/database.cjs
@@ -0,0 +1,12 @@
+module.exports={
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: '1596874130AxZ',
+    database: 'dev-burger-db',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }}
